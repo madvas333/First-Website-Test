56d2f7b6c749a46e94c6dd446aea05a0560cf236
Madison Vasquez <madvas333@gmail.com>
2024-07-27 11:46:18 -0700

    testing my changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import {section} from 'react';
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h1>  Austin first </h1>
+      </div>
+
+
     </div>
   );
 }
