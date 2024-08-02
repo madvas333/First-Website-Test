a7b6c7919982c41aec34d6a1dd7302be70213542
Madison Vasquez <madvas333@gmail.com>
2024-08-02 12:06:03 -0700

    added home pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './HomePage.js';  
+import AboutPage from './AboutPage.js'; 
 import './App.css';
+import logo from './logo.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Madison Extravaganza</p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+// src/HomePage.js
+
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+export default HomePage;
