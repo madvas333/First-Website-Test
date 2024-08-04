5b6ba6cbd1bf171a188ed541e949a492426b6f30
Madison Vasquez <madvas333@gmail.com>
2024-08-03 21:19:05 -0700

    more styling to CSS

--- src/App.js
+++ src/App.js
@@ -7,37 +7,30 @@ import logo from './logo.jpeg';
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="fabulous-text">Madison Extravaganza</p>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="header-text">Madison Extravaganza</h1>
+        <p className="subtext">Just in case Instagram somehow disappears</p>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </main>
-      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,15 +3,14 @@ import image1 from './images/draw1.jpeg';
 import image2 from './images/draw2.jpeg';
 import image3 from './images/draw3.jpeg';
 import image4 from './images/draw4.jpeg';
-import './App.css'; // Ensure your CSS file is imported
+import image5 from './images/draw5.jpeg';
+import './App.css';
 
 function HomePage() {
   return (
     <div className="HomePage">
-      <h1 className="fabulous-text">Welcome to the Home Page</h1>
-      {/* Shooting Stars Container */}
+      <h1 className="header-text">Madison Extravaganza</h1>
       <div className="shooting-stars">
-        {/* Create 10 shooting star elements with random positions and delays */}
         {[...Array(10)].map((_, i) => (
           <div
             key={i}
@@ -26,12 +25,13 @@ function HomePage() {
       </div>
 
       <main>
-        <h2>Image Gallery</h2>
+        <h2>My Art</h2>
         <div className="image-gallery">
           <img src={image1} alt="Draw 1" className="gallery-image" />
-          <img src={image2} alt="Draw 2" className="gallery-image" />
+          <img src={image5} alt="Draw 5" className="gallery-image" />
           <img src={image3} alt="Draw 3" className="gallery-image" />
           <img src={image4} alt="Draw 4" className="gallery-image" />
+          <img src={image2} alt="Draw 2" className="gallery-image" />
         </div>
       </main>
     </div>
